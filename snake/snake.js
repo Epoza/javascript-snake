5e9c7bce6e4294ba517ab50164625594a522f2c0
Epoza <86025026+Epoza@users.noreply.github.com>
2022-03-10 17:58:28 -0500

    Add files via upload

---
+++ snake/snake.js
@@ -0,0 +1,28 @@
+export const SNAKE_SPEED = 5
+const snakeBody = [
+    {x: 10, y: 11},
+    {x: 11, y: 11},
+    {x: 12, y: 11}
+]
+
+export function update() {
+    // gets second to last snake part
+    for (let i = snakeBody.length - 2; i >= 0; i--) {
+        snakeBody[i + 1] = { ...snakeBody[i] }
+    }
+
+    snakeBody[0].x += 1
+    snakeBody[0].y += 0
+}
+
+export function draw(gameBoard) {
+    snakeBody.forEach(segment => {
+        const snakeElement = document.createElement('div')
+        snakeElement.style.gridRowStart = segment.y
+        snakeElement.style.gridColumnStart = segment.x
+        snakeElement.classList.add('snake')
+        gameBoard.appendChild(snakeElement)
+    })
+}
+
+// try on linux
